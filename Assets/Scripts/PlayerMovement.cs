aeb066ee47b240ad4baa65f6b6ebd834f7f45eb4
DAVID_SAMOSIR\david <daviddenovannn@gmail.com>
2023-09-14 13:10:56 +0700

    no message

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,27 +6,45 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    // components
     [SerializeField] bool hasControl;
     public static PlayerMovement localPlayer;
-
     Rigidbody myRB;
-    Transform myAvatar;
     Animator myAnim;
+    Transform myAvatar;
+    
+    // player movement
     [SerializeField] InputAction WASD;
     Vector2 movementInput;
     [SerializeField] float movementSpeed;
-
+    
+    // player color
     static Color myColor;
     SpriteRenderer myAvatarSprite;
+
+    // role
+    [SerializeField] bool isImposter; 
+    [SerializeField] InputAction KILL;
+
+    PlayerMovement target;
+    [SerializeField] Collider myCollider;
+
+    bool IsDead;
+
+    private void Awake()
+    {
+        KILL.performed += KillTarget;
+    }
 
     private void OnEnable()
     {
         WASD.Enable();
+        KILL.Enable();
     }
     private void OnDisable()
     {
         WASD.Disable();
+        KILL.Disable();
     }
 
     // Start is called before the first frame update
@@ -37,12 +56,13 @@ public class PlayerMovement : MonoBehaviour
         }
 
         myRB = GetComponent<Rigidbody>();
-        myAvatar = transform.GetChild(0);
         myAnim = GetComponent<Animator>();
+        myAvatar = transform.GetChild(0);
         myAvatarSprite = myAvatar.GetComponent<SpriteRenderer>();
-
         if(myColor == Color.clear)
             myColor = Color.white;
+        if(!hasControl)
+            return;
         myAvatarSprite.color = myColor;
 
     }
@@ -50,6 +70,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!hasControl)
+            return;
+
         movementInput = WASD.ReadValue<Vector2>();
         if (movementInput.x != 0)
         {
@@ -71,5 +94,54 @@ public class PlayerMovement : MonoBehaviour
         {
             myAvatarSprite.color = newColor;
         }
+    }
+
+    public void SetRole(bool newRole)
+    {
+        isImposter = newRole;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag =="Player")
+        {
+            PlayerMovement tempTarget = other.GetComponent<PlayerMovement>();
+            if(isImposter)
+            {
+                if(tempTarget.isImposter)
+                    return;
+                else
+                {
+                    target = tempTarget;
+                    // Debug.Log(target.name);
+                }
+            }
+        }
+    }
+
+    void KillTarget(InputAction.CallbackContext context)
+    {
+
+        if (context.phase == InputActionPhase.Performed)
+        {
+            if (target == null)
+                return;
+            else
+            {
+                if(target.IsDead)
+                    return;
+                transform.position = target.transform.position;
+                target.Die();
+                target = null;
+            }
+        }
+    }
+
+    public void Die()
+    {
+        IsDead = true;
+
+        myAnim.SetBool("IsDead", IsDead);
+        myCollider.enabled = false;
     }
 }
