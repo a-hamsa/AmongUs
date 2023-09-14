bd6111320cfb66a4367a5979574ef54e91df74aa
Ahmad Hamsa <72251778+a-hamsa@users.noreply.github.com>
2023-09-14 13:26:42 +0700

    Revert "no message"

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,48 +5,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // components
+
     [SerializeField] bool hasControl;
     public static PlayerMovement localPlayer;
+
     Rigidbody myRB;
-    Animator myAnim;
     Transform myAvatar;
-    
-    // player movement
+    Animator myAnim;
     [SerializeField] InputAction WASD;
     Vector2 movementInput;
     [SerializeField] float movementSpeed;
-    
-    // player color
+
     static Color myColor;
     SpriteRenderer myAvatarSprite;
-
-    // role
-    [SerializeField] bool isImposter; 
-    [SerializeField] InputAction KILL;
-
-    PlayerMovement target;
-    [SerializeField] Collider myCollider;
-
-    bool IsDead;
-
-    private void Awake()
-    {
-        KILL.performed += KillTarget;
 
     static Sprite myAccsSprite;
     SpriteRenderer myAccsHolder;
 
-
     private void OnEnable()
     {
         WASD.Enable();
-        KILL.Enable();
     }
     private void OnDisable()
     {
         WASD.Disable();
-        KILL.Disable();
     }
 
     // Start is called before the first frame update
@@ -59,18 +40,13 @@ public class PlayerMovement : MonoBehaviour
         }
 
         myRB = GetComponent<Rigidbody>();
-        myAnim = GetComponent<Animator>();
         myAvatar = transform.GetChild(0);
+        myAnim = GetComponent<Animator>();
         myAvatarSprite = myAvatar.GetComponent<SpriteRenderer>();
-
-
         myAccsHolder = transform.GetChild(1).GetComponent<SpriteRenderer>();
-
 
         if(myColor == Color.clear)
             myColor = Color.white;
-        if(!hasControl)
-            return;
         myAvatarSprite.color = myColor;
 
 
@@ -83,9 +59,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!hasControl)
-            return;
-
         movementInput = WASD.ReadValue<Vector2>();
         if (movementInput.x != 0)
         {
@@ -108,54 +81,6 @@ public class PlayerMovement : MonoBehaviour
             myAvatarSprite.color = newColor;
         }
     }
-
-    public void SetRole(bool newRole)
-    {
-        isImposter = newRole;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag =="Player")
-        {
-            PlayerMovement tempTarget = other.GetComponent<PlayerMovement>();
-            if(isImposter)
-            {
-                if(tempTarget.isImposter)
-                    return;
-                else
-                {
-                    target = tempTarget;
-                    // Debug.Log(target.name);
-                }
-            }
-        }
-    }
-
-    void KillTarget(InputAction.CallbackContext context)
-    {
-
-        if (context.phase == InputActionPhase.Performed)
-        {
-            if (target == null)
-                return;
-            else
-            {
-                if(target.IsDead)
-                    return;
-                transform.position = target.transform.position;
-                target.Die();
-                target = null;
-            }
-        }
-    }
-
-    public void Die()
-    {
-        IsDead = true;
-
-        myAnim.SetBool("IsDead", IsDead);
-        myCollider.enabled = false;
 
     public void SetAccs(Sprite newAccs)
     {
