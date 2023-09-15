b9a8ee01a92f1b36c6b39ca7d0cae5f33ec032ea
DESKTOP-GFGQUA8\Anastasia <ahmadhamsa01@gmail.com>
2023-09-15 14:46:26 +0700

    Finish

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,26 +9,22 @@ public class PlayerMovement : MonoBehaviour
     // components
     [SerializeField] bool hasControl;
     public static PlayerMovement localPlayer;
-
     [SerializeField] Rigidbody myRB;
     [SerializeField] Animator myAnim;
     [SerializeField] Transform myAvatar;
     
-
-    CapsuleCollider cpsColider;
-
     // player movement
     [SerializeField] InputAction WASD;
     Vector2 movementInput;
     [SerializeField] float movementSpeed;
-
+    
     // player color
     static Color myColor;
     SpriteRenderer myAvatarSprite;
 
 
     // role
-    [SerializeField] bool isImposter;
+    [SerializeField] bool isImposter; 
     [SerializeField] InputAction KILL;
 
     PlayerMovement target;
@@ -41,8 +37,8 @@ public class PlayerMovement : MonoBehaviour
         KILL.performed += KillTarget;
     }
 
-    //static Sprite myAccsSprite;
-    //SpriteRenderer myAccsHolder;
+    static Sprite myAccsSprite;
+    SpriteRenderer myAccsHolder;
 
     private void OnEnable()
     {
@@ -68,21 +64,19 @@ public class PlayerMovement : MonoBehaviour
         myAvatar = transform.GetChild(0);
         myAvatarSprite = myAvatar.GetComponent<SpriteRenderer>();
 
-
         // myAccsHolder = transform.GetChild(1).GetComponent<SpriteRenderer>();
 
 
-
-        if (myColor == Color.clear)
+        if(myColor == Color.clear)
             myColor = Color.white;
-        if (!hasControl)
+        if(!hasControl)
             return;
         myAvatarSprite.color = myColor;
 
 
-        //if (myAccsSprite != null)
-        //    myAccsHolder.sprite = myAccsSprite;
-
+        if(myAccsSprite != null)
+            myAccsHolder.sprite = myAccsSprite;
+        
 
     }
 
@@ -97,19 +91,19 @@ public class PlayerMovement : MonoBehaviour
         {
             myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
         }
-
+        
         myAnim.SetFloat("Speed", movementInput.magnitude);
     }
 
     private void FixedUpdate()
     {
-        myRB.velocity = movementInput * movementSpeed;
+        myRB.velocity = movementInput * movementSpeed; 
     }
 
     public void SetColor(Color newColor)
     {
         myColor = newColor;
-        if (myAvatarSprite != null)
+        if(myAvatarSprite != null)
         {
             myAvatarSprite.color = newColor;
         }
@@ -123,12 +117,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag =="Player")
         {
             PlayerMovement tempTarget = other.GetComponent<PlayerMovement>();
-            if (isImposter)
+            if(isImposter)
             {
-                if (tempTarget.isImposter)
+                if(tempTarget.isImposter)
                     return;
                 else
                 {
@@ -148,7 +142,7 @@ public class PlayerMovement : MonoBehaviour
                 return;
             else
             {
-                if (target.IsDead)
+                if(target.IsDead)
                     return;
                 transform.position = target.transform.position;
                 target.Die();
@@ -157,15 +151,12 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    public void SetAccs(Sprite newAccs)
+    {
+        myAccsSprite = newAccs;
+        myAccsHolder.sprite = myAccsSprite;
 
-=======
-    //public void SetAccs(Sprite newAccs)
-    //{
-    //    myAccsSprite = newAccs;
-    //    myAccsHolder.sprite = myAccsSprite;
-//
-    //}
-
+    }
     public void Die()
     {
         IsDead = true;
@@ -173,7 +164,5 @@ public class PlayerMovement : MonoBehaviour
         myAnim.SetBool("IsDead", IsDead);
         myCollider.enabled = false;
 
-        cpsColider.isTrigger = false;
-        
     }
 }
